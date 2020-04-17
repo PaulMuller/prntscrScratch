5e3afb5a499e57269f9edce5c151613f50de84ae
PaulMuller <Melnyk300@gmail.com>
2020-04-17 20:57:03 +0300

    update for Viktor

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const fetch = require('node-fetch')
 const https = require('https')
 const fs = require('fs')
 const crypto = require("crypto")
-var AWS = require('aws-sdk')
-const proxyList = require("./proxylist")
-var proxy = require('proxy-agent')
 
 url = 'https://prnt.sc/'
 regexp_full = /screenshot-image" src="(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi
@@ -12,7 +9,7 @@ regexp_final = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]
 counter = 0
 
 const downloadRandom = async() => {
-    const id = crypto.randomBytes(3).toString('hex')
+    const id = `rt4` + crypto.randomBytes(1).toString('hex') + crypto.randomBytes(1).toString('hex').slice(1)
     console.log(id)
 
     await fetch(`${url}${id}`,{method: "get"})
@@ -32,14 +29,10 @@ const downloadRandom = async() => {
 const main = () => {
     i=0
     const id = setInterval(() => {
-        AWS.config.update({
-            httpOptions: { agent: proxy("http://" + proxyList[i].host) }
-        })
-        console.log(AWS.config.httpOptions.agent.proxyUri)
         downloadRandom()
         i>50 ?i =0: i++
         if (counter > 500) clearInterval(id)
-    }, 2000);
+    }, 500);
 }
 main()
 
